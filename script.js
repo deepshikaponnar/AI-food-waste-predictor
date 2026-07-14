6b83bb57587c0f3fa36c44ea774c05833beb64c5
Deepshika P <deepshikaponnar08@gmail.com>
2026-07-14 22:18:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Welcome to FlowSenses!");
-});
-
-function showPrediction() {
-    alert("AI Food Waste Prediction feature will be added soon!");
-}
